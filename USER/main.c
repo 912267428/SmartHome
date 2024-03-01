c41cc9d5fa3db3dfa3b5fedf687ad5eb697d1720
YuanKe <912267428@qq.com>
2024-03-01 15:58:58 +0800

    starting up
    增加开机界面，解决温湿度传感器通电后一段时间没有度数已经光照传感器可能在通电后第一时间度数极大导致错误报警的情况。

--- USER/main.c
+++ USER/main.c
@@ -19,32 +19,13 @@ extern u8 alarm_is_free = 10; //报警是否被手动操作
 u8 humiH, humiL, tempH, tempL;
 float light;
 
+void Hardware_Init(void);
+void OLED_ShowFixed(void);
+
 int main(void)
 {	
-	delay_init();	    //延时函数初始化
-	Usart1_Init(115200);	//调试串口
-	Usart2_Init(115200);	//stm32 to esp8266通讯串口
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
-	LED_Init();		  	//初始化与LED连接的硬件接口
-	Buzzer_Init();
-	KEY_Init();
-	EXTIX_Init();
-	
-	OLED_Init();
-	DHT11_Init();
-	BH1750_Init();
-	
-	OLED_Clear();
-	OLED_ShowString(1,1,"Smart Home");
-	OLED_ShowString(2,1,"humi:  .");
-	OLED_ShowString(3,1,"temp:  .");
-	OLED_ShowString(4,1,"Light:      lx");
-	
-	TIM2_Int_Init(4999, 7199);
-	TIM3_Int_Init(2499, 7199);
-	printf("Hardware init OK\r\n");
-	
+	Hardware_Init();
+	OLED_ShowFixed();
 	
 	while(1) 
 	{	
@@ -80,3 +61,38 @@ int main(void)
 	}
  }
 
+void Hardware_Init(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	delay_init();	    //延时函数初始化
+	
+	Usart1_Init(115200);	//调试串口
+	Usart2_Init(115200);	//stm32 to esp8266通讯串口
+	
+	LED_Init();		  	//初始化与LED连接的硬件接口
+	Buzzer_Init();
+	KEY_Init();
+	OLED_Init();
+	DHT11_Init();
+	BH1750_Init();
+	
+	EXTIX_Init();
+	
+	TIM2_Int_Init(4999, 7199);
+	TIM3_Int_Init(2499, 7199);
+	
+	OLED_ShowString(1,1,"Starting Up");
+	delay_ms(300);
+	printf("Hardware init OK\r\n");
+	OLED_ShowString(1,1,"Completed");
+	delay_ms(200);
+}
+
+void OLED_ShowFixed(void)
+{
+	OLED_Clear();
+	OLED_ShowString(1,1,"Smart Home");
+	OLED_ShowString(2,1,"humi:  .");
+	OLED_ShowString(3,1,"temp:  .");
+	OLED_ShowString(4,1,"Light:      lx");
+}
